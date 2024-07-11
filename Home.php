1a70459d78cc52aa3f7801b7c5cc190130dc39b1
Matt P <149812326+G1tG0t619@users.noreply.github.com>
2024-07-11 18:15:52 -0400

    Add files via upload
    Note: I am currently undertaking efforts to convert existing HTML code into PHP format, as well as implementing CSS code into each file.

---
+++ Home.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>W3.CSS Template</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inconsolata">
+        <style>
+        body, html {
+            height: 100%;
+            font-family: "Inconsolata", sans-serif;
+        }
+
+        .bgimg {
+            background-position: center;
+            background-size: cover;
+            background-image: url("https://www.w3schools.com/w3images/coffeehouse.jpg");
+            min-height: 75%;
+        }
+
+        .menu {
+            display: none;
+        }
+    </style>
+  </head>
+    </head>
+    <body>
+        <div class = "div1">
+            <h1 class  = "firsth1">ReturnIt<h1>
+        </div>
+        
+        <div class = "navbar">
+            <ul class = "centered-list">
+                <li><a class = "active">Home</a></li>
+                <li><a href = "About.php"></a>About</li>
+                <li><a href = "Return.php"></a>Make a Return</li>
+                <li><a href = "History.php"></a>Return History</li>
+                <li><a href = "Contact.php"></a>Contact Us</li>
+            </ul>
+        </div>
+        <hr>
+
+        <div class="info">
+            <h1>About Us</h1>
+            <p style="font-size: larger;">
+                Founded in January of 2024, ReturnIt is a company that specializes in providing fast and convenient return services.
+                Whether you're a small business owner or a customer with a item that's proven too challenging to return, ReturnIt can give you the help you need! 
+                Customers will have the option to either drop-off their products at a designated ReturnIt store, or to register them for pickup. 
+                We will work with companies to ensure that all products are returned efficiently and with no hassle.
+            </p>
+        </div>
+        <hr>
+
+    </body>
+</html>
